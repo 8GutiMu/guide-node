c81694b21a6c7cb3816d639309d6c8a83e562b95
8GutiMu <you@example.com>
2017-07-11 14:44:12 -0500

    servidor arriba

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.use('/static', express.static(path.join(__dirname,'node_modules')));
 app.use('/static', express.static(path.join(__dirname,'assets')));
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname+"index.html")
+res.sendFile(__dirname+"/index.html")
 });
 
 
-app.listen(8080);
+app.listen(8580);
